49ca93e4445b998efe87df231c648f3d6fbc9561
JWood <j.r.wood@hotmail.co.uk>
2018-02-08 21:33:53 +0000

    Initial commit

---
+++ Cerberus.Tests/Cerberus.Tests/Controllers/PasswordHashControllerTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Cerberus.Tests.Cerberus.Tests.Controllers
+{
+    using global::Cerberus.Contracts.Controllers;
+    using global::Cerberus.Controllers;
+    using System;
+    using Xunit;
+
+    public class PasswordHashControllerTests
+    {
+        private readonly IPasswordHashController _passwordHashController;
+        private const string Password = "Password1234!";
+
+        public PasswordHashControllerTests()
+        {
+            _passwordHashController = new PasswordHashController();
+        }
+
+        [Fact]
+        public void Controller_IsOfCorrectType()
+        {
+            Assert.IsAssignableFrom<IPasswordHashController>(_passwordHashController);
+        }
+
+        [Fact]
+        public void Controller_HashString_NullPasswordThrowsException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => _passwordHashController.HashString(null));
+            Assert.Equal("Value cannot be null.\r\nParameter name: Password", ex.Message);
+        }
+
+        [Fact]
+        public void Controller_HashString_HashesValueAsExpected()
+        {
+            var result = _passwordHashController.HashString(Password);
+
+            Assert.Equal(68, result.Length);
+        }
+    }
+}
---
+++ Cerberus/Controllers/PasswordHashController.cs
@@ -0,0 +1,67 @@
+﻿namespace Cerberus.Controllers
+{
+    using Cerberus.Contracts.Controllers;
+    using System;
+    using System.Security.Cryptography;
+
+    public class PasswordHashController : IPasswordHashController
+    {
+        private const int SaltLength = 20;
+        private const int HashLength = 30;
+        private const int IntIterations = 10000;
+
+        public string HashString(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("Password");
+            }
+            else
+            {
+                byte[] salt;
+                new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltLength]);
+
+                var pbkdf2 = new Rfc2898DeriveBytes(password, salt, IntIterations);
+                var hash = pbkdf2.GetBytes(HashLength);
+
+                var hashBytes = new byte[SaltLength + HashLength];
+                Array.Copy(salt, 0, hashBytes, 0, SaltLength);
+                Array.Copy(hash, 0, hashBytes, SaltLength, HashLength);
+
+                var savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+                return savedPasswordHash;
+            }
+        }
+
+        public bool ValidatePassword(string password, string hashedPassword)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("Password");
+            }
+
+            if (string.IsNullOrEmpty(hashedPassword))
+            {
+                throw new ArgumentNullException("HashedPassword");
+            }
+
+            var hashBytes = Convert.FromBase64String(hashedPassword);
+            var salt = new byte[SaltLength];
+            Array.Copy(hashBytes, 0, salt, 0, SaltLength);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, IntIterations);
+            var hash = pbkdf2.GetBytes(HashLength);
+
+            for (var i = 0; i < HashLength; i++)
+            {
+                if (hashBytes[i + SaltLength] != hash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
